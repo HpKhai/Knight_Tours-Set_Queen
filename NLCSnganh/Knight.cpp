21b9737c443b6aa6b0d69369d4cccc3d10ccaaf1
HpKhai <152875969+HpKhai@users.noreply.github.com>
2024-05-14 17:07:11 +0700

    Add files via upload

--- NLCSnganh/Knight.cpp
+++ NLCSnganh/Knight.cpp
@@ -2,17 +2,15 @@
 #include <vector>
 #include <utility>
 #include <fstream>
+#include <cmath>
 using namespace std;
 
-// Kích thu?c c?a b?ng
 #define N 5
 
-// Hàm ki?m tra xem ô (x, y) có n?m trong b?ng và chua du?c di qua chua
 bool isSafe(int x, int y, vector<vector<int> >& board) {
     return (x >= 0 && y >= 0 && x < N && y < N && board[x][y] == -1);
 }
 
-// In b?ng ra màn hình
 void printBoard(vector<vector<int> >& board) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -22,13 +20,11 @@ void printBoard(vector<vector<int> >& board) {
     }
 }
 
-// Hàm gi?i bài toán di tu?n c?a quân mã
 bool solveKTUtil(vector<vector<int> >& board, int x, int y, int movei, int xMove[], int yMove[]) {
     int k, next_x, next_y;
-    if (movei == N * N  ) // N?u dã di qua h?t các ô
+    if (movei == 25) // N?u dã di qua h?t các ô
         return true;
 
-    // Th? t?t c? các bu?c di chuy?n ti?p theo t? (x, y)
     for (k = 0; k < 8; k++) {
         next_x = x + xMove[k];
         next_y = y + yMove[k];
@@ -37,37 +33,72 @@ bool solveKTUtil(vector<vector<int> >& board, int x, int y, int movei, int xMove
             if (solveKTUtil(board, next_x, next_y, movei + 1, xMove, yMove))
                 return true;
             else
-                board[next_x][next_y] = -1; // N?u không thành công, dánh d?u ô này chua di qua
+                board[next_x][next_y] = -1;
         }
     }
     return false;
 }
 
-
-bool solveKT(vector<pair<int, int> >& initialPositions) {
-    vector<vector<int> > board(N, vector<int>(N, -1)); // Kh?i t?o b?ng
-    // Các bu?c di chuy?n c?a quân mã
+vector<vector<int> > solveKT(vector<pair<int, int> >& initialPositions) {
+    vector<vector<int> > board(N, vector<int>(N, -1));
     int xMove[] = {2, 1, -1, -2, -2, -1, 1, 2};
     int yMove[] = {1, 2, 2, 1, -1, -2, -2, -1};
 
-	// L?y giá tr? c?a quân h?u cu?i cùng t? danh sách initialPositions
     int lastKnightIndex = initialPositions.size() - 1;
     int x = initialPositions[lastKnightIndex].first;
     int y = initialPositions[lastKnightIndex].second;
-	
-    // Ðánh d?u t?t c? các ô dã du?c d?t quân mã tru?c dó là dã di qua
+
     for (int i = 0; i < initialPositions.size(); ++i) {
         int x = initialPositions[i].first;
         int y = initialPositions[i].second;
-        board[x][y] = i; // Giá tr? c?a quân mã là s? th? t? c?a nó trong danh sách initialPositions
+        board[x][y] = i;
     }
-	int lastKnightValue =lastKnightIndex + 1;
-//	printBoard(board);
-    // Th? gi?i bài toán di tu?n t? quân h?u cu?i cùng du?c nh?p vào
+    int lastKnightValue = lastKnightIndex + 1;
+
     if (solveKTUtil(board, x, y, lastKnightValue, xMove, yMove)) {
-    	cout<<"This is solutions!"<<endl;
+        cout << "This is the solution:" << endl;
         printBoard(board);
-        ofstream outFile("solution-boardKningt.txt");
+        return board;
+    }
+
+    cout << "Solution does not exist" << endl;
+    return vector<vector<int> >();
+}
+
+bool isKnightMove(pair<int, int> pos1, pair<int, int> pos2) {
+    int dx = abs(pos1.first - pos2.first);
+    int dy = abs(pos1.second - pos2.second);
+    return (dx == 2 && dy == 1) || (dx == 1 && dy == 2);
+}
+
+int main() {
+    vector<pair<int, int> > initialPositions;
+    int n;
+    cout << "Enter the number of initial knight-positions: ";
+    cin >> n;
+
+    cout << "Enter the initial positions (x, y):" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Enter the initial positions " << i + 1 << " (x, y): ";
+        int x, y;
+        cin >> x >> y;
+        initialPositions.push_back(make_pair(x, y));
+    }
+
+    // G?i hàm d? gi?i quy?t bài toán
+    vector<vector<int> > board = solveKT(initialPositions);
+
+    // Ki?m tra t?t c? các v? trí trong m?ng initialPositions có di theo quy t?c quân mã không
+    bool validMoves = true;
+    for (size_t i = 1; i < initialPositions.size(); ++i) {
+        if (!isKnightMove(initialPositions[i - 1], initialPositions[i])) {
+            validMoves = false;
+            cout << "Invalid knight move from position " << i << " to position " << i + 1 << "." << endl;
+        }
+    }
+
+    if (validMoves && !board.empty()) {
+        ofstream outFile("solution-boardQueen.txt");
         if (outFile.is_open()) {
             // Ghi bàn c? vào t?p tin
             for (int i = 0; i < N; i++) {
@@ -78,31 +109,10 @@ bool solveKT(vector<pair<int, int> >& initialPositions) {
             }
             // Ðóng t?p tin
             outFile.close();
-            cout << "The chessboard has been recorded in the file solution-boardKningt.txt." << endl;
-    	}
-        return true;
+            cout << "The chessboard has been recorded in the file solution-boardQueen.txt." << endl;
+        }
     }
 
-    cout << "Solution does not exist";
-    return false;
-}
-
-// Hàm main
-int main() {
-    int n;
-    cout << "Enter the number of initial knight-positions: ";
-    cin >> n;
-
-    vector<pair<int, int> > initialPositions;
-//    cout << "Enter the initial positions (x, y):" << endl;
-    for (int i = 0; i < n; i++) {
-    	cout << "Enter the initial positions "<< i+1 <<" (x, y):" << endl;
-        int x, y;
-        cin >> x >> y;
-        initialPositions.push_back(make_pair(x, y));
-    }
-
-    solveKT(initialPositions);
     return 0;
 }
 
